dd2ef6eda799c3b76a650c40c165a48eed2d6b5f
Robson Kretzmann da Silva <robsonkretzmann@hotmail.com>
2024-04-19 08:59:04 -0300

    update

--- src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
+++ src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class MercadoLivroApplication
 
-fun main(args: Array<String>) {
+	fun main(args: Array<String>) {
 	runApplication<MercadoLivroApplication>(*args)
 }
